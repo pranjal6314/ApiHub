b16e5176ff6eada6c2ebb60235f1523c70ca1ff1
Pranjal Choudhary <pranjalchoudhary270@gmail.com>
2023-06-27 18:47:05 +0530

    Implement Button component with variant styles and loading indicator

---
+++ src/components/SignOutButton.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react'
+
+interface SignOutButtonProps {
+  
+}
+
+const SignOutButton: FC<SignOutButtonProps> = ({}) => {
+  return <div>SignOutButton</div>
+}
+
+export default SignOutButton
