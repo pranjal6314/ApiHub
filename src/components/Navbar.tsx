f4b8cfed3f6947a50b25b43e7a45c33e4a489b81
Pranjal Choudhary <pranjal.choudhary2020@vitstudent.ac.in>
2023-06-30 19:46:55 +0530

    by pranjal choudhary

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -14,7 +14,7 @@ console.log('session', session)
     <div className='fixed backdrop-blur-sm bg-white/75 dark:bg-slate-900/75 z-50 top-0 left-0 right-0 h-20 border-b border-slate-300 dark:border-slate-700 shadow-sm flex items-center justify-between'>
       <div className='container max-w-7xl mx-auto w-full flex justify-between items-center'>
         <Link href='/' className={buttonVariants({ variant: 'link' })}>
-          Text & Number v1.0
+          Text & Number v1.0  <span className='text-sm text-red-400 hover:text-orange-400' >&nbsp; by Pranjal</span>
         </Link>
 
         <div className='md:hidden'>
